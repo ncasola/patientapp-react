5f45f542f21da0a7c546c49eec892293f61035ce
Néstor Casola Jiménez <ncasolajimenez@gmail.com>
2022-11-15 22:53:47 +0000

    Dockerized

--- src/_helpers/customBaseQuery.js
+++ src/_helpers/customBaseQuery.js
@@ -1,28 +1,34 @@
-import { fetchBaseQuery } from '@reduxjs/toolkit/query'
-import { store } from '_store'
-import { authActions } from '_store';
+import { fetchBaseQuery } from "@reduxjs/toolkit/query";
+import { store } from "_store";
+import { authActions } from "_store";
 let url = "";
 // check if jest is running
-const isJest = typeof jest !== 'undefined';
+const isJest = typeof jest !== "undefined";
 // check if production
-const isProduction = process.env.NODE_ENV === 'production';
+const isProduction = process.env.NODE_ENV === "production";
 // check if development
-const isDevelopment = process.env.NODE_ENV === 'development';
-if(isJest) {
-    url = process.env.REACT_APP_API_URL_LOCAL;
+const isDevelopment = process.env.NODE_ENV === "development";
+if (isJest) {
+  url = process.env.REACT_APP_API_URL_LOCAL;
 }
-if(isProduction) {
-    url = process.env.REACT_APP_API_URL;
+if (isProduction) {
+  url = process.env.REACT_APP_API_URL;
 }
-if(isDevelopment) {
-    url = "/api";
+if (isDevelopment) {
+  url = "/api";
 }
-const baseQuery = fetchBaseQuery({ baseUrl: url });
+const baseQuery = fetchBaseQuery({
+  baseUrl: url,
+  prepareHeaders(headers) {
+    return headers;
+  },
+  credentials: "include",
+});
 export const baseQueryWithauth = async (args, api, extraOptions) => {
-    let result = await baseQuery(args, api, extraOptions)
-    if (result.error && result.error.originalStatus === 401) {
-        store.dispatch(authActions.logout());
-    } else {
-        return result;
-    }
-}
+  let result = await baseQuery(args, api, extraOptions);
+  if (result.error && result.error.originalStatus === 401) {
+    store.dispatch(authActions.logout());
+  } else {
+    return result;
+  }
+};
